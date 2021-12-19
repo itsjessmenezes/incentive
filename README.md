13b368cf1ce2f16c790e48d262027094a767bbae
Jessica Menezes <jessicamenezes833@gmail.com>
2021-12-19 20:07:01 -0300

    README

---
+++ README.md
@@ -0,0 +1,9 @@
+##Inspiração do projeto:
+Este projeto foi inspirado em duas experiências: a de conseguir uma bolsa de estudos patrocinada pelo iFood, estudando com cursos gratuitos;
+a oportunidade de participar de um hackathon onde o meu grupo criou um projeto que ajudasse pessoas através do conhecimento.
+
+Hoje em dia vemos que existem muitos cursos gratuitos, porém, falta direcionamento, qualidade e informação de como e por onde seguir.
+A maioria dos devs "perde tempo" sem saber por onde começar ou não sabem a existencia de um curso excelente que está disponível gratuitamente.
+Esta plataforma foi desenvolvida com intuito de unir cursos gratuitos conceituados, ordenados de alguma forma, que ajuda o estudante a tomar um caminho mais direcionado. Não é um site de estudo, e sim de divulgação de escolas ou cursos. E para que a plataforma alcance mais pessoas implementei um incentivo a quem divulga.
+
+Os links de direcionamento dos cursos são exemplos e foram escolhidos porque em algum momento de minha tragetória fui ajudada por esta escola e/ou professor(a).
---
+++ script.js
@@ -0,0 +1,149 @@
+const coursesHome = [
+  {
+    course: 'Design',
+    quantity: 21
+  },
+  {
+    course: 'Empreendedorismo',
+    quantity: 09
+  },
+  {
+    course: 'Finanças',
+    quantity: 32
+  },
+  {
+    course: 'Marketing',
+    quantity: 14
+  },
+  {
+    course: 'Negócios',
+    quantity: 29
+  },
+  {
+    course: 'Tecnologia',
+    quantity: 53
+  },
+];
+
+const communityCompany = [
+  'Rocketseat',
+  'iFood',
+  'TRANSvest',
+  'Educa TRANSforma',
+  'Cubos Academy',
+  'Provi',
+  'PretUX',
+  'NuBank',
+  'PrograMaria',
+  'Mercado Livre',
+  'VTEX',
+  'XP Inc'
+];
+
+const nivelAwardsCards = [
+  {
+    nivel: 1,
+    description: 'Certificado de voluntariado'
+  },
+  {
+    nivel: 2,
+    description: 'Descontos nos cursos pagos de nossos parceiros'
+  },
+  {
+    nivel: 3,
+    description: 'Participação dos sorteios de cursos e livros da plataforma'
+  },
+  {
+    nivel: 4,
+    description: 'Perfil em destaque. Indicação para vagas das empresas parceiras'
+  },
+  {
+    nivel: 5,
+    description: 'Auxilio carreira. Monitoria para auxiliar com a carreira que desejar seguir'
+  },
+];
+
+const peopleImpacted = ['LEVEL UP', 10, 20, 40, 80, 100];
+
+const encouragersCard = [
+  {
+    name: 'Rafael Garcia',
+    impact: 52,
+    awards: {
+      certificated: true,
+      award: 3,
+      featured: false
+    },
+    trophy: 3
+  },
+  {
+    name: 'Lara Almeida',
+    impact: 73,
+    awards: {
+      certificated: true,
+      award: 2,
+      featured: false
+    },
+    trophy: 3
+  },
+  {
+    name: 'Luiz Carlos',
+    impact: 92,
+    awards: {
+      certificated: true,
+      award: 4,
+      featured: false
+    },
+    trophy: 4
+  },
+  {
+    name: 'Debora Melo',
+    impact: 23,
+    awards: {
+      certificated: true,
+      award: 0,
+      featured: false
+    },
+    trophy: 2
+  },
+  {
+    name: 'Rodrigo Leite',
+    impact: 12,
+    awards: {
+      certificated: true,
+      award: 0,
+      featured: false
+    },
+    trophy: 1
+  },
+  {
+    name: 'Meire Lima',
+    impact: 34,
+    awards: {
+      certificated: true,
+      award: 0,
+      featured: false
+    },
+    trophy: 2
+  },
+  {
+    name: 'Jonas Matos',
+    impact: 44,
+    awards: {
+      certificated: true,
+      award: 1,
+      featured: false
+    },
+    trophy: 3
+  },
+  {
+    name: 'Duda Brum',
+    impact: 97,
+    awards: {
+      certificated: true,
+      award: 5,
+      featured: true
+    },
+    trophy: 4
+  },
+];
